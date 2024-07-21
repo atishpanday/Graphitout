976e2178303aef0be35b13bd617cbafc7dc9f228
Atish Panday <atishpanday@Atishs-MacBook-Air.local>
2024-07-20 17:03:21 -0700

    added gh-pages deployment config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-    basePath: isProd ? '/Graphitout' : '',
-    assetPrefix: isProd ? '/Graphitout/' : '',
-    trailingSlash: true,
-    output: 'export',
-};
+const nextConfig = {};
 
 export default nextConfig;
